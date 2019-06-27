73ca2122d0257fb40237cbe2c94470ec7ce7fbc6
Arthemus <arthemus.moreira@gmail.com>
2019-06-28 09:30:19 +1000

    Fixing env.

--- src/config.ts
+++ src/config.ts
@@ -8,5 +8,5 @@ export const dbSchema = process.env.DATABASE_SCHEMA || 'spacenow-api-users';
 export const dbUsername = process.env.DATABASE_USERNAME || 'spacenowtest';
 export const dbPassword = process.env.DATABASE_PASSWORD || 'Spac.918273!';
 export const dbEndpoint =
-  process.env.DATABASE_ENDPOINT ||
+  process.env.DATABASE_HOST ||
   'spacenow-test.cjo4zy3wnflc.ap-southeast-2.rds.amazonaws.com';
