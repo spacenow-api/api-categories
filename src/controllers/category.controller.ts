6f797dfb6836e692d6e5ef3ec89d21a5bf8ea7ec
Arthemus <arthemus.moreira@gmail.com>
2019-07-02 10:04:01 +1000

    Formating and fixing error log.

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,49 +1,62 @@
 import { Router, Request, Response, NextFunction } from 'express';
+
 import sequelizeErrorMiddleware from '../helpers/middlewares/sequelize-error-middleware';
 import authMiddleware from '../helpers/middlewares/auth-middleware';
 import ICategory from './category.interface';
-import { Category } from '../models';
- 
-class CategoryController {
 
+import { Category } from '../models';
+
+class CategoryController {
   public path = '/categories';
   public router = Router();
-  
+
   constructor() {
     this.intializeRoutes();
   }
- 
+
   private intializeRoutes() {
     this.router.get(this.path, this.getRootCategories);
     this.router.get(`${this.path}/:id`, this.getCategory);
     this.router.post(this.path, authMiddleware, this.createCategory);
     this.router.patch(this.path, authMiddleware, this.createCategory);
   }
- 
-  private getRootCategories = async (request: Request, response: Response, next: NextFunction) => {
+
+  private getRootCategories = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
     try {
-      const categories: ICategory[] = await Category.findAll({ where: {parentId: null} });
+      const categories: ICategory[] = await Category.findAll({
+        where: { parentId: null }
+      });
       response.send(categories);
     } catch (error) {
-      console.log(error)
       sequelizeErrorMiddleware(error, request, response, next);
     }
-  }
+  };
 
-  private getCategory = async (request: Request, response: Response, next: NextFunction) => {
+  private getCategory = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
     try {
-      const category: ICategory = await Category.findOne({ 
-        where: {id: request.params.id}, 
+      const category: ICategory = await Category.findOne({
+        where: { id: request.params.id },
         include: [{ model: Category, as: 'children' }]
       });
       response.send(category);
     } catch (error) {
-      console.log(error)
       sequelizeErrorMiddleware(error, request, response, next);
     }
-  }
- 
-  private createCategory = async (request: Request, response: Response, next: NextFunction) => {
+  };
+
+  private createCategory = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
     const data = request.body;
     try {
       const category = await Category.create(data);
@@ -51,8 +64,7 @@ class CategoryController {
     } catch (error) {
       sequelizeErrorMiddleware(error, request, response, next);
     }
-  }
-
+  };
 }
- 
+
 export default CategoryController;
