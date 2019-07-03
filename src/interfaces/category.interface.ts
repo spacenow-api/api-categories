be03aab59f1cdc395fc291ea42dcc4b19694c107
Arthemus <arthemus.moreira@gmail.com>
2019-07-03 16:17:07 +1000

    Fixing interfaces.

--- src/interfaces/category.interface.ts
+++ src/interfaces/category.interface.ts
@@ -22,8 +22,22 @@ interface ISubCategoryLegacy {
   id: number;
   listSettingsParentId: number;
   listSettingsChildId: number;
+  typeId: number;
+  itemName: string;
+  otherItemName: string;
+  description: string;
+  maximum: number;
+  minimum: number;
+  startValue: number;
+  endValue: number;
+  step: string;
+  isEnable: string;
+  photo: string;
+  photoType: string;
+  isSpecification: number;
   createdAt: Date;
   updatedAt: Date;
+  specData: string;
   bookingPeriod: IBookingPeriodLegacy;
 }
 
